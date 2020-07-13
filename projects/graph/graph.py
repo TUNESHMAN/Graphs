cdb18fdbf0fb99c11dc521612445db4aac592dd6
Babatunde <babatundea15@gmail.com>
2020-07-13 12:35:08 +0100

    fixed the add method

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -3,9 +3,11 @@ Simple graph implementation
 """
 from util import Stack, Queue  # These may come in handy
 
+
 class Graph:
 
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
+
     def __init__(self):
         self.vertices = {}
 
@@ -13,13 +15,14 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        pass  # TODO
+        self.vertices[vertex_id] = set()  #
 
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:  # I add edge from v1 to v2. However, I first check if they are both in the graph
+            self.vertices[v1].add(v2)  # add v2 to v1 set of edges
 
     def get_neighbors(self, vertex_id):
         """
@@ -75,6 +78,7 @@ class Graph:
         This should be done using recursion.
         """
         pass  # TODO
+
 
 if __name__ == '__main__':
     graph = Graph()  # Instantiate your graph
